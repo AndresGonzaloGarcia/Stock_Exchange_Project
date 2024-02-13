c564fb2a64614b51c5d580da23bf6bda63864cf1
AndresGonzaloGarcia <garciaandresgonzalo@gmail.com>
2024-02-13 14:50:13 -0300

    added stock tracker webpage

--- stock_website/urls.py
+++ stock_website/urls.py
@@ -5,5 +5,6 @@ from website import views
 urlpatterns = [
     
     path('admin/', admin.site.urls),
-    path('', views.home, name= 'home')
+    path('', views.home, name= 'home'),
+    path('stock_tracker/', views.stock_tracker, name= 'stock_tracker'),
 ]
--- website/views.py
+++ website/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html', {})
+
+def stock_tracker(request):
+    return render(request, 'stock_tracker.html', {})
